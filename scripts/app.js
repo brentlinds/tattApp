d5b54db799c2bb027dcb513bda2be3ac99d9e1fd
Lindsay Woods <lindsaywoods@live.ca>
2018-02-15 12:12:38 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+const app = {}
+
+app.events = () => {
+    $("form").on("submit", function (e) {
+        // prevents browser default behaviour
+        e.preventDefault();
+        // creates a variable to hold user's input
+        const favething = $("input").val();
+        console.log(favething);
+        // passing user's input into the function that retrieves their icon
+        app.newIcon(favething)
+    })
+}
+
+app.newIcon = (query) => {
+        return $.ajax({
+            url: "https://noun-project-proxy.herokuapp.com/v1",
+            method: 'GET',
+            data: {
+                url: `icons/${query}`,
+                params: JSON.stringify({
+
+                })
+            }
+        }).then(function (response) {
+            // console.log(response);
+            // create variable to randomize the object selected out of the returned array
+            const random = response.icons[Math.floor(Math.random() * response.icons.length)]
+            // console.log(random);
+            let image = random.preview_url;
+            console.log(image);
+            $(".results").html(`<img src= ${image}>`);
+
+        });
+}
+
+
+app.init = () => {
+    app.events();
+}
+
+$(function(){
+    app.init();
+
+});
